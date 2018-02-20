b059ea85cb9fd74a5441397ed2edb11f192069d5
JustineDiza <justinediza@gmail.com>
2018-02-20 16:12:20 +0800

    Made first webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+// If environment used is production,
+// minify and do not include sourcemapping,
+// otherwise it is in debug mode 
+// Sourcemapping == to view consoles/use dev tool
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  devtool: debug ? "inline-sourcemap" : false,
+  entry: "./js/scripts.js",
+  output: {
+    path: __dirname + "/js",
+    filename: "scripts.min.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
