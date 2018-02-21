e5d1b13f0972859f1928961d404061e4841a9a25
JustineDiza <justinediza@gmail.com>
2018-02-21 17:42:38 +0800

    Modified webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,31 @@
 // Sourcemapping == to view consoles/use dev tool
 var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
-  context: __dirname,
+  context: path.join(__dirname, "src"),
   devtool: debug ? "inline-sourcemap" : false,
   entry: "./js/scripts.js",
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+        }
+      }
+    ]
+  },
   output: {
-    path: __dirname + "/js",
+    path: __dirname + "/src",
     filename: "scripts.min.js"
   },
   plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
   ],
