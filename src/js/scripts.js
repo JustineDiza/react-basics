d4d609370dfa86b5cc957fb99ab053fe4975f4c6
JustineDiza <justinediza@gmail.com>
2018-02-26 17:06:45 +0800

    Basic components for react with comments

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,10 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class Layout extends React.Component {
+	constructor() {
+		super(); // Pre-requisite whenever you have React.Component extension
+		this.getHobbies = 'write poems.';
+	}
+
+	// Creating methods in a class
+	getAge(age) {
+		return `Justine and she's ${age} years old!`; // Using string templating
+	}
+
 	render() {
-		const name = "Justine";
+		const name = 'Justine';
 		return (
-			<h1>It's {name}!</h1>
+			// JSX must be wrapped in an enclosing tag, only one parent DOM element should appear
+			<div>
+				<h1>It's {this.getAge(23)}</h1> {/* Anything in {} will be executed as normal javascript */}
+				<h2>She likes to {this.getHobbies}</h2>
+			</div>
 		);
 	}
 }
