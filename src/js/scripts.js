50065eca6e8ca1ba77b36e0371fd7a98bac54bdd
JustineDiza <justinediza@gmail.com>
2018-02-26 16:43:27 +0800

    Modified script to show react coding

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,14 @@
-require('./module1.js');
-require('./module2.js');
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component {
+	render() {
+		return (
+			<h1>It's working!</h1>
+		);
+	}
+}
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<Layout/>, app);
